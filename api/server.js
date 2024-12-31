8533b20f00d09311d59776ae423bc077e92e771e
Neesui <nanumaya2012@gmail.com>
2024-12-31 11:01:04 +0545

    connect to db

--- api/server.js
+++ api/server.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Database connection Succesfully")
+})
+    .catch((err) => {
+        console.log(err)
+    })
 
 const app = express();
 
