bed112833245d320c5c21bb1d20c9eba325f2056
Neesui <neeshachy2020@gmail.com>
2025-06-24 00:40:37 +0545

    change in authControllers

--- Auth/controllers/authControllers.js
+++ Auth/controllers/authControllers.js
@@ -2,7 +2,7 @@ import User from "../models/userModels.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const register = async (req, res) => {
+export const register = async (req, res) => {
     try {
         const { username, password, role } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -16,7 +16,7 @@ const register = async (req, res) => {
     }
 };
 
-const login = async (req, res) => {
+export const login = async (req, res) => {
     try {
         const { username, password } = req.body;
         const user = await User.findOne({ username });
@@ -41,5 +41,3 @@ const login = async (req, res) => {
         res.status(500).json({ message: "Something went wrong", error: err.message });
     }
 };
-
-export default { register, login };
